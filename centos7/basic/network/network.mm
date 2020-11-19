423f921d8614c82303852e73e135ce3081ce0714
gwaysoft <gwaysoft@outlook.com>
2020-11-19 17:25:01 +0800

    shell

---
+++ centos7/basic/network/network.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1605769530799" ID="ID_1233702988" MODIFIED="1605769546266" TEXT="network"/>
+</map>
